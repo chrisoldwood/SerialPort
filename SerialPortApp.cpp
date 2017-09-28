00da0558d093ad7cbaa744d91bd5339dbac88b24
Chris Oldwood <gort@cix.co.uk>
2017-09-28 23:21:11 +0100

    Added initial code to read from stdin and send it down a COM port.

--- SerialPortApp.cpp
+++ SerialPortApp.cpp
@@ -7,6 +7,9 @@
 #include "SerialPortApp.hpp"
 #include <WCL/Path.hpp>
 #include <WCL/VerInfoReader.hpp>
+#include <WCL/Win32Exception.hpp>
+#include <Core/CmdLineException.hpp>
+#include <Core/AnsiWide.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 // The table of command line switches.
@@ -15,13 +18,15 @@ enum
 {
 	USAGE	= 0,	//!< Show the program options syntax.
 	VERSION	= 1,	//!< Show the program version and copyright.
+	PORT	= 2,	//!< The COM port number.
 };
 
 static Core::CmdLineSwitch s_switches[] = 
 {
-	{ USAGE,	TXT("?"),	NULL,			Core::CmdLineSwitch::ONCE,	Core::CmdLineSwitch::NONE,	NULL,	TXT("Display the program options syntax")	},
-	{ USAGE,	TXT("h"),	TXT("help"),	Core::CmdLineSwitch::ONCE,	Core::CmdLineSwitch::NONE,	NULL,	TXT("Display the program options syntax")	},
-	{ VERSION,	TXT("v"),	TXT("version"),	Core::CmdLineSwitch::ONCE,	Core::CmdLineSwitch::NONE,	NULL,	TXT("Display the program version")			},
+	{ USAGE,	TXT("?"),	NULL,			Core::CmdLineSwitch::ONCE,	Core::CmdLineSwitch::NONE,		NULL,			TXT("Display the program options syntax")	},
+	{ USAGE,	TXT("h"),	TXT("help"),	Core::CmdLineSwitch::ONCE,	Core::CmdLineSwitch::NONE,		NULL,			TXT("Display the program options syntax")	},
+	{ VERSION,	TXT("v"),	TXT("version"),	Core::CmdLineSwitch::ONCE,	Core::CmdLineSwitch::NONE,		NULL,			TXT("Display the program version")			},
+	{ PORT,		TXT("p"),	TXT("port"),	Core::CmdLineSwitch::ONCE,	Core::CmdLineSwitch::SINGLE,	TXT("port"),	TXT("The COM port number to write to")		},
 };
 static size_t s_switchCount = ARRAY_SIZE(s_switches);
 
@@ -43,7 +48,7 @@ SerialPortApp::~SerialPortApp()
 ////////////////////////////////////////////////////////////////////////////////
 //! Run the application.
 
-int SerialPortApp::run(int argc, tchar* argv[], tistream& /*in*/, tostream& out, tostream& /*err*/)
+int SerialPortApp::run(int argc, tchar* argv[], tistream& in, tostream& out, tostream& /*err*/)
 {
 	m_parser.parse(argc, argv, Core::CmdLineParser::ALLOW_UNIX_FORMAT);
 
@@ -59,6 +64,28 @@ int SerialPortApp::run(int argc, tchar* argv[], tistream& /*in*/, tostream& out,
 		showVersion(out);
 		return EXIT_SUCCESS;
 	}
+
+	if (!m_parser.isSwitchSet(PORT))
+		throw Core::CmdLineException(TXT("No COM port number specified [--port]"));
+
+	const uint portNumber = Core::parse<uint>(m_parser.getSwitchValue(PORT));
+
+	if ((portNumber < 1) || (portNumber > 9) )
+		throw Core::CmdLineException(TXT("Invalid COM port number, expecting 1-9"));
+
+	const tstring filename = Core::fmt(TXT("\\\\.\\COM%u"), portNumber); 
+	const HANDLE device = ::CreateFile(filename.c_str(), GENERIC_READWRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+	if (device == INVALID_HANDLE_VALUE)
+		throw WCL::Win32Exception(::GetLastError(), TXT("Failed to open serial port"));
+
+	for (tstring line; std::getline(in, line);)
+	{
+		const std::string buffer = T2A(line);
+		if (!::WriteFile(device, buffer.data(), buffer.size(), nullptr, nullptr))
+			throw WCL::Win32Exception(::GetLastError(), TXT("Failed to write to serial port"));
+	}
+
+	::CloseHandle(device);
 
 	return EXIT_SUCCESS;
 }
